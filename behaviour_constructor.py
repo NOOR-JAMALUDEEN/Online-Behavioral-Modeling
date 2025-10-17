9fd5dec4181a6942ae6234c89c10ee02f851f3f9
NJD <43238903+NOOR-JAMALUDEEN@users.noreply.github.com>
2025-10-17 13:53:54 +0200

    Update behaviour_constructor.py

--- behaviour_constructor.py
+++ behaviour_constructor.py
@@ -1,3 +1,13 @@
+Copyright (c) 2025 Noor Jamaludeen
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://spdx.org/licenses/Apache-2.0.html
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 import pandas as pd
 import numpy as np
 from scipy.stats import t
@@ -1221,4 +1231,5 @@ class behaviourConstructor:
     def load_rskf_splits(self, path: str ):
         split_list= joblib.load(Path(path))
         return split_list
+
 
